c985de4ad5ab97c894e0ddabdc37d19b84b73276
Jules <github@sirskunkalot.de>
2021-05-21 00:46:04 +0200

    cleanup project

---
+++ PlanBuild/PatcherEquipmentQuickSlots.cs
@@ -0,0 +1,40 @@
+﻿using EquipmentAndQuickSlots;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBuild
+{
+    class PatcherEquipmentQuickSlots
+    {
+
+        [HarmonyPatch(typeof(PlanBuild), "ReplaceHammerInInventory")]
+        [HarmonyPrefix]
+        static bool PlanBuild_ReplaceHammerInInventory_Prefix(PlanBuild __instance)
+        {
+            Player player = Player.m_localPlayer;
+            ExtendedInventory extendedInventory = player.GetInventory() as ExtendedInventory;
+            extendedInventory.CallBase = true;
+            try
+            {
+                foreach(Inventory inventory in extendedInventory._inventories)
+                { 
+                    if (__instance.ReplaceHammer(player, inventory))
+                    {
+                        return false;
+                    }
+                }
+            }
+            finally
+            {
+                extendedInventory.CallBase = false;
+            }
+
+            return false;
+        }
+         
+    }
+}
