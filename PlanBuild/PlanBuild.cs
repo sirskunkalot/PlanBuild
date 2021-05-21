625aebe75894c8424ea22a187c04f015a4d5bf5b
Jules <github@sirskunkalot.de>
2021-05-21 22:13:32 +0200

    bump version to 0.2.0

--- PlanBuild/PlanBuild.cs
+++ PlanBuild/PlanBuild.cs
@@ -29,7 +29,7 @@ namespace PlanBuild
     {
         public const string PluginGUID = "marcopogo.PlanBuild";
         public const string PluginName = "PlanBuild";
-        public const string PluginVersion = "0.1.7";
+        public const string PluginVersion = "0.2.0";
 
         public const string PlanBuildButton = "PlanBuild_PlanBuildMode";
 
