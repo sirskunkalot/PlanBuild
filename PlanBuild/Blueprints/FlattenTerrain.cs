5a6b643d4d465b4b89df1d417c120a6ffa607904
Mathias <mathias.decrock@gmail.com>
2021-07-16 22:04:20 +0200

    revert back to bounds.min.y

--- PlanBuild/Blueprints/FlattenTerrain.cs
+++ PlanBuild/Blueprints/FlattenTerrain.cs
@@ -16,35 +16,13 @@ namespace PlanBuild.Blueprints
             { 
                 try
                 {
-                    Quaternion groundOrientation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-                    Vector3 boxColliderHalfExtents = new Vector3(0.5f, bounds.extents.y, 0.5f);
-                    int layerMask = LayerMask.GetMask("piece", "piece_nonsolid");
+                    Quaternion groundOrientation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0); 
                     //TerrainModifier.SetTriggerOnPlaced(true);
                     for (float localZ = bounds.min.z; localZ < bounds.max.z; localZ++)
                     { 
                         for(float localX = bounds.min.x; localX < bounds.max.x; localX++)
-                        {
-                            Vector3 groundTargetLocation = transform.TransformPoint(new Vector3(localZ, bounds.min.y, localX));
-                            Collider[] colliders = Physics.OverlapBox(transform.TransformPoint(new Vector3(localZ, bounds.center.y, localX)), boxColliderHalfExtents, groundOrientation, layerMask);
-                            if(colliders == null || colliders.Length == 0)
-                            {
-                                continue;
-                            }
-                            float lowestColliderHeight = float.MaxValue;
-                            foreach(Collider collider in colliders)
-                            {
-                                Piece piece = collider.GetComponentInParent<Piece>();
-                                if(!piece || !pieces.Contains(piece.gameObject))
-                                {
-                                    continue;
-                                }
-                                lowestColliderHeight = Mathf.Min(lowestColliderHeight, collider.bounds.min.y);
-                            }
-                            if(lowestColliderHeight == float.MaxValue)
-                            {
-                                continue;
-                            }
-                            groundTargetLocation.y = lowestColliderHeight - 0.5f;
+                        { 
+                            Vector3 groundTargetLocation = transform.TransformPoint(new Vector3(localZ, bounds.min.y -0.5f, localX)); 
                             Object.Instantiate(groundPrefab, groundTargetLocation, groundOrientation); 
                         } 
                     }
