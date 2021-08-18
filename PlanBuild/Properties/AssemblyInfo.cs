50ac33edd46cb4cccc5fae3b85c2457d3443b787
Jules <github@sirskunkalot.de>
2021-08-18 21:19:38 +0200

    changelog

--- PlanBuild/Properties/AssemblyInfo.cs
+++ PlanBuild/Properties/AssemblyInfo.cs
@@ -28,8 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.4.2.0")]
 [assembly: AssemblyVersion("0.4.2.0")]
 [assembly: AssemblyFileVersion("0.4.2.0")]
