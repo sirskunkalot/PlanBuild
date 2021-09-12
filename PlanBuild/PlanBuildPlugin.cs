9a1d6483cb0362a6cca3282c7b8201938a8c22c1
Jules <github@sirskunkalot.de>
2021-09-12 19:32:21 +0200

    use and depend on jötunn v2.2.9

--- PlanBuild/PlanBuildPlugin.cs
+++ PlanBuild/PlanBuildPlugin.cs
@@ -17,7 +17,7 @@ using ShaderHelper = PlanBuild.Utils.ShaderHelper;
 namespace PlanBuild
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-    [BepInDependency(Jotunn.Main.ModGuid, "2.2.8")]
+    [BepInDependency(Jotunn.Main.ModGuid, "2.2.9")]
     [BepInDependency(Patches.BuildCameraGUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency(Patches.CraftFromContainersGUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency(Patches.GizmoGUID, BepInDependency.DependencyFlags.SoftDependency)]
