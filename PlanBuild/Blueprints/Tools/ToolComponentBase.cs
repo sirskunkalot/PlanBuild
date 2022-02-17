bef664ad3bf7ea38ed344f95f94cb0cc9325899b
Jules <github@sirskunkalot.de>
2022-02-17 19:29:29 +0100

    fix placement offset

--- PlanBuild/Blueprints/Tools/ToolComponentBase.cs
+++ PlanBuild/Blueprints/Tools/ToolComponentBase.cs
@@ -12,16 +12,16 @@ namespace PlanBuild.Blueprints.Tools
         public static Vector3 PlacementOffset = Vector3.zero;
 
         internal bool SuppressPieceHighlight = true;
+        internal bool ResetPlacementOffset = true;
 
         private void Start()
         {
-            // if (!Player.m_localPlayer || Player.m_localPlayer.GetSelectedPiece() == null ||
-            //     !gameObject.name.StartsWith(Player.m_localPlayer.GetSelectedPiece().gameObject.name))
-            // {
-            //     return;
-            // }
-
             OnStart();
+            
+            if (ResetPlacementOffset)
+            {
+                PlacementOffset = Vector3.zero;
+            }
 
             On.Player.UpdateWearNTearHover += Player_UpdateWearNTearHover;
             On.Player.PieceRayTest += Player_PieceRayTest;
