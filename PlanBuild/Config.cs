279126ce2f46de86b565bf9404bf7571ab7b37b9
Jules <github@sirskunkalot.de>
2022-06-25 02:58:23 +0200

    fix config order for bep manager

--- PlanBuild/Config.cs
+++ PlanBuild/Config.cs
@@ -77,34 +77,34 @@ namespace PlanBuild
             AllowDirectBuildConfig = PlanBuildPlugin.Instance.Config.Bind(
                 ServerSection, "Allow direct build", false,
                 new ConfigDescription($"Allow placement of blueprints without materials on this server.{Environment.NewLine}Admins are always allowed to use it.", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
 
             AllowTerrainmodConfig = PlanBuildPlugin.Instance.Config.Bind(
                 ServerSection, "Allow terrain tools", false,
                 new ConfigDescription($"Allow usage of the terrain modification tools on this server.{Environment.NewLine}Admins are always allowed to use them.", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
 
             AllowServerBlueprints = PlanBuildPlugin.Instance.Config.Bind(
                 ServerSection, "Allow serverside blueprints", false,
                 new ConfigDescription($"Allow sharing of blueprints on this server.", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
 
             AllowMarketHotkey = PlanBuildPlugin.Instance.Config.Bind(
                 ServerSection, "Allow clients to use the GUI toggle key", true,
                 new ConfigDescription($"Allow clients to use the Hotkey to access server blueprints.{Environment.NewLine}Admins are always allowed to use it.", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
             
             PlanBlacklistConfig = PlanBuildPlugin.Instance.Config.Bind(
                 ServerSection, "Excluded plan prefabs", "AltarPrefab,FloatingIslandMO",
                 new ConfigDescription($"Comma separated list of prefab names to exclude from the planned piece table on this server{Environment.NewLine}Admins are always allowed to build them.", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order, Browsable = false }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order, Browsable = false }));
 
             // Rune Section
 
             DefaultBuildModeConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Default build mode", DefaultBuildMode.Planned,
                 new ConfigDescription("Default build mode when placing blueprints.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             DefaultBuildModeConfig.SettingChanged += (sender, args) =>
             {
@@ -117,117 +117,117 @@ namespace PlanBuild
             UnlimitedHealthConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Unlimited health", false,
                 new ConfigDescription("Set Piece health to its maximum value when directly building blueprints.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             RayDistanceConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Place distance", 50f,
                 new ConfigDescription("Place distance while using the Blueprint Rune.",
                     new AcceptableValueRange<float>(8f, 80f),
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             CameraOffsetIncrementConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Camera offset increment", 2f,
                 new ConfigDescription("Camera height change when holding the camera modifier key and scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             InvertCameraOffsetScrollConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Invert camera offset scroll", false,
                 new ConfigDescription("Invert the direction of camera offset scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             PlacementOffsetIncrementConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Placement offset increment", 0.1f,
                 new ConfigDescription("Placement height change when holding the modifier key and scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             InvertPlacementOffsetScrollConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Invert placement height change scroll", false,
                 new ConfigDescription("Invert the direction of placement offset scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             SelectionIncrementConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Selection increment", 1f,
                 new ConfigDescription("Selection radius increment when scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             InvertSelectionScrollConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Invert selection scroll", false,
                 new ConfigDescription("Invert the direction of selection scrolling.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             SelectionConnectedMarginConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Selection connected check margin", 0.01f,
                 new ConfigDescription("Distance of the shell used to check for connectedness.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             ShowGridConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Show the transform bound grid", false,
                 new ConfigDescription("Shows a grid around the blueprints' bounds to visualize the blueprints' edges.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             TooltipEnabledConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Tooltip enabled", true,
                 new ConfigDescription("Show a tooltip with a bigger thumbnail for blueprint pieces.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             TooltipBackgroundConfig = PlanBuildPlugin.Instance.Config.Bind(
                 RuneSection, "Tooltip Color", new Color(0.13f, 0.13f, 0.13f, 0.65f),
                 new ConfigDescription("Set the background color for the tooltip on blueprint pieces.", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             // Directory Section
 
             BlueprintSearchDirectoryConfig = PlanBuildPlugin.Instance.Config.Bind(
                 DirectorySection, "Search directory", ".",
                 new ConfigDescription("Base directory to scan (recursively) for blueprints and vbuild files, relative paths are relative to the valheim.exe location", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             BlueprintSaveDirectoryConfig = PlanBuildPlugin.Instance.Config.Bind(
                 DirectorySection, "Save directory", "BepInEx/config/PlanBuild/blueprints",
                 new ConfigDescription("Directory to save blueprint files, relative paths are relative to the valheim.exe location", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             // Keybind Section
 
             MarketHotkeyConfig = PlanBuildPlugin.Instance.Config.Bind(
                 KeybindSection, "Blueprint Marketplace GUI toggle key", KeyCode.End,
                 new ConfigDescription("Hotkey to show the blueprint marketplace GUI", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             ShiftModifierConfig = PlanBuildPlugin.Instance.Config.Bind(
                 KeybindSection, "ShiftModifier", KeyCode.LeftShift,
                 new ConfigDescription("First modifier key to change behaviours on various tools (defaults to LeftShift)", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             CtrlModifierConfig = PlanBuildPlugin.Instance.Config.Bind(
                 KeybindSection, "CtrlModifier", KeyCode.LeftControl,
                 new ConfigDescription("Second modifier key to change behaviours on various tools (defaults to LeftCtrl)", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             AltModifierConfig = PlanBuildPlugin.Instance.Config.Bind(
                 KeybindSection, "AltModifier", KeyCode.LeftAlt,
                 new ConfigDescription("Third modifier key to change behaviours on various tools (defaults to LeftAlt)", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             ToggleConfig = PlanBuildPlugin.Instance.Config.Bind(
                 KeybindSection, "Toggle", KeyCode.Q,
                 new ConfigDescription("Key to switch between modes on various tools", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             // Plans Section
 
             ShowAllPieces = PlanBuildPlugin.Instance.Config.Bind(
                 PlansSection, "Plan unknown pieces", false,
                 new ConfigDescription("Show all plans, even for pieces you don't know yet", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
             RadiusConfig = PlanBuildPlugin.Instance.Config.Bind(
                 PlansSection, "Plan totem build radius", 30f,
                 new ConfigDescription("Build radius of the plan totem", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
             ShowParticleEffects = PlanBuildPlugin.Instance.Config.Bind(
                 PlansSection, "Plan totem particle effects", true,
                 new ConfigDescription("Show particle effects when building pieces with the plan totem", null,
-                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = ++order }));
+                    new ConfigurationManagerAttributes { IsAdminOnly = true, Order = --order }));
 
             ShowAllPieces.SettingChanged += (obj, attr) => PlanManager.UpdateKnownRecipes();
             //RadiusConfig.SettingChanged += (_, _) => PlanManager.UpdateAllPlanTotems();  // that doesnt change the radius...
@@ -238,23 +238,23 @@ namespace PlanBuild
             ConfigTransparentGhostPlacement = PlanBuildPlugin.Instance.Config.Bind(
                 VisualSection, "Transparent Ghost Placement", true,
                 new ConfigDescription("Apply plan shader to ghost placement (currently placing piece)", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
             UnsupportedColorConfig = PlanBuildPlugin.Instance.Config.Bind(
                 VisualSection, "Unsupported color", new Color(1f, 1f, 1f, 0.1f),
                 new ConfigDescription("Color of unsupported plan pieces", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
             SupportedPlanColorConfig = PlanBuildPlugin.Instance.Config.Bind(
                 VisualSection, "Supported color", new Color(1f, 1f, 1f, 0.5f),
                 new ConfigDescription("Color of supported plan pieces", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
             TransparencyConfig = PlanBuildPlugin.Instance.Config.Bind(
                 VisualSection, "Transparency", 0.30f,
                 new ConfigDescription("Additional transparency for finer control.", new AcceptableValueRange<float>(0f, 1f),
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
             GlowColorConfig = PlanBuildPlugin.Instance.Config.Bind(
                 VisualSection, "Plan totem glow color", Color.cyan,
                 new ConfigDescription("Color of the glowing lines on the Plan totem", null,
-                    new ConfigurationManagerAttributes { Order = ++order }));
+                    new ConfigurationManagerAttributes { Order = --order }));
 
             ConfigTransparentGhostPlacement.SettingChanged += UpdateGhostPlanPieceTextures;
             UnsupportedColorConfig.SettingChanged += UpdateAllPlanPieceTextures;
