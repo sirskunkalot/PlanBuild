9e3c2ee5508248e9a2a5f86ecf0996bc15ba34d1
Jules <github@sirskunkalot.de>
2022-07-21 21:38:50 +0200

    manual release v0.10.3

--- PlanBuild/PlanBuildPlugin.cs
+++ PlanBuild/PlanBuildPlugin.cs
@@ -28,7 +28,7 @@ namespace PlanBuild
     {
         public const string PluginGUID = "marcopogo.PlanBuild";
         public const string PluginName = "PlanBuild";
-        public const string PluginVersion = "0.10.2";
+        public const string PluginVersion = "0.10.3";
 
         public static PlanBuildPlugin Instance;
         
