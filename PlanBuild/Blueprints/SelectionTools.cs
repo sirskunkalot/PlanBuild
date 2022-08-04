23c8d156f2ec566e566f412ef41c91beede41f18
Jules <github@sirskunkalot.de>
2022-08-04 22:50:15 +0200

    qol for the bp save gui

--- PlanBuild/Blueprints/SelectionTools.cs
+++ PlanBuild/Blueprints/SelectionTools.cs
@@ -49,7 +49,7 @@ namespace PlanBuild.Blueprints
                 selection.Clear();
                 return;
             }
-            ShowSaveGUI(bpname, (name, category, description) =>
+            SaveGUI.ShowSaveGUI(bpname, (name, category, description) =>
             {
                 if (string.IsNullOrEmpty(name))
                 {
@@ -93,6 +93,9 @@ namespace PlanBuild.Blueprints
                 bp.CreateThumbnail();
                 BlueprintManager.RegisterKnownBlueprints();
                 BlueprintGUI.ReloadBlueprints(BlueprintLocation.Local);
+            }, () =>
+            {
+                selection.Clear();
             });
         }
         
@@ -115,186 +118,227 @@ namespace PlanBuild.Blueprints
             var action = new UndoActions.UndoRemove(ZDOs);
             UndoManager.Instance.Add(Config.BlueprintUndoQueueNameConfig.Value, action);
         }
-        
-        private static void ShowSaveGUI(string bpname, Action<string, string, string> okAction)
+
+        private static class SaveGUI
         {
-            var panel = GUIManager.Instance.CreateWoodpanel(
-                parent: GUIManager.CustomGUIFront.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0f, 0f),
-                width: 600,
-                height: 500,
-                draggable: false);
-            panel.SetActive(false);
+            private static Action<string, string, string> OkAction;
+            private static Action CancelAction;
+            private static GameObject Panel;
+            private static InputField Name;
+            private static InputField Category;
+            private static InputField Description;
 
-            var layout = panel.AddComponent<VerticalLayoutGroup>();
-            layout.childAlignment = TextAnchor.MiddleCenter;
-            layout.childForceExpandWidth = true;
-            layout.childForceExpandHeight = false;
-            layout.padding = new RectOffset(15, 15, 15, 15);
-            layout.spacing = 20f;
-            
-            // Label
-
-            var saveLabel = GUIManager.Instance.CreateText(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_saveblueprint"),
-                parent: panel.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0f, 0f),
-                font: GUIManager.Instance.AveriaSerifBold,
-                fontSize: 30,
-                color: GUIManager.Instance.ValheimOrange,
-                outline: true,
-                outlineColor: Color.black,
-                width: 500f,
-                height: 50f,
-                addContentSizeFitter: false);
-            saveLabel.GetComponent<Text>().alignment = TextAnchor.UpperCenter;
-            saveLabel.AddComponent<LayoutElement>().preferredHeight = 80f;
-
-            // Name
-
-            var name = new GameObject("Name");
-            name.transform.SetParent(panel.transform);
-            name.AddComponent<LayoutElement>().preferredHeight = 70f;
-
-            var nameLabel = GUIManager.Instance.CreateText(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_name"),
-                parent: name.transform,
-                anchorMin: new Vector2(0f, 1f),
-                anchorMax: new Vector2(0f, 1f),
-                position: new Vector2(100f, 0f),
-                font: GUIManager.Instance.AveriaSerif,
-                fontSize: 20,
-                color: GUIManager.Instance.ValheimOrange,
-                outline: true,
-                outlineColor: Color.black,
-                width: 150f,
-                height: 40f,
-                addContentSizeFitter: false);
-
-            var nameInput = GUIManager.Instance.CreateInputField(
-                parent: name.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0f, 0f),
-                contentType: InputField.ContentType.Standard,
-                placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_name_placeholder"),
-                fontSize: 20,
-                width: 400f,
-                height: 40f);
-            
-            var nameValue = nameInput.GetComponent<InputField>();
-            nameValue.text = bpname;
-
-            // Category
-
-            var cat = new GameObject("Category");
-            cat.transform.SetParent(panel.transform);
-            cat.AddComponent<LayoutElement>().preferredHeight = 70f;
-
-            var catLabel = GUIManager.Instance.CreateText(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_category"),
-                parent: cat.transform,
-                anchorMin: new Vector2(0f, 1f),
-                anchorMax: new Vector2(0f, 1f),
-                position: new Vector2(100f, 0f),
-                font: GUIManager.Instance.AveriaSerif,
-                fontSize: 20,
-                color: GUIManager.Instance.ValheimOrange,
-                outline: true,
-                outlineColor: Color.black,
-                width: 150f,
-                height: 40f,
-                addContentSizeFitter: false);
-
-            var catInput = GUIManager.Instance.CreateInputField(
-                parent: cat.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0f, 0f),
-                contentType: InputField.ContentType.Alphanumeric,
-                placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_category_placeholder"),
-                fontSize: 20,
-                width: 400f,
-                height: 40f);
-
-            var catValue = catInput.GetComponent<InputField>();
-            catValue.text = BlueprintAssets.CategoryBlueprints;
-            
-            // Description
-
-            var desc = new GameObject("Description");
-            desc.transform.SetParent(panel.transform);
-            desc.AddComponent<LayoutElement>().preferredHeight = 170f;
-
-            var descLabel = GUIManager.Instance.CreateText(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_description"),
-                parent: desc.transform,
-                anchorMin: new Vector2(0f, 1f),
-                anchorMax: new Vector2(0f, 1f),
-                position: new Vector2(100f, 0f),
-                font: GUIManager.Instance.AveriaSerif,
-                fontSize: 20,
-                color: GUIManager.Instance.ValheimOrange,
-                outline: true,
-                outlineColor: Color.black,
-                width: 150f,
-                height: 40f,
-                addContentSizeFitter: false);
-
-            var descInput = GUIManager.Instance.CreateInputField(
-                parent: desc.transform,
-                anchorMin: new Vector2(0.5f, 0.5f),
-                anchorMax: new Vector2(0.5f, 0.5f),
-                position: new Vector2(0f, 0f),
-                contentType: InputField.ContentType.Standard,
-                placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_description_placeholder"),
-                fontSize: 20,
-                width: 400f,
-                height: 120f);
-
-            var descValue = descInput.GetComponent<InputField>();
-            descValue.lineType = InputField.LineType.MultiLineNewline;
-
-            // Buttons
-
-            var buttons = new GameObject("Buttons");
-            buttons.transform.SetParent(panel.transform);
-            buttons.AddComponent<LayoutElement>().preferredHeight = 80f;
-
-            var okButton = GUIManager.Instance.CreateButton(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_confirm"),
-                parent: buttons.transform,
-                anchorMin: new Vector2(0f, 0.5f),
-                anchorMax: new Vector2(0f, 0.5f),
-                position: new Vector2(100f, 0f),
-                width: 100f,
-                height: 40f); 
-            okButton.GetComponent<Button>().onClick.AddListener(() => OnClick(okAction));
-
-            var cancelButton = GUIManager.Instance.CreateButton(
-                text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_cancel"),
-                parent: buttons.transform,
-                anchorMin: new Vector2(1f, 0.5f),
-                anchorMax: new Vector2(1f, 0.5f),
-                position: new Vector2(-100f, 0f),
-                width: 100f,
-                height: 40f);
-            cancelButton.GetComponent<Button>().onClick.AddListener(() => OnClick(null));
-
-            void OnClick(Action<string, string, string> action)
+            public static void ShowSaveGUI(string bpname, Action<string, string, string> okAction, Action cancelAction)
             {
-                action?.Invoke(nameValue.text.Trim(), catValue.text.Trim(), descValue.text.Trim());
-                panel.SetActive(false);
-                Object.Destroy(panel);
+                OkAction = okAction;
+                CancelAction = cancelAction;
+
+                // Panel
+
+                Panel = GUIManager.Instance.CreateWoodpanel(
+                    parent: GUIManager.CustomGUIFront.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0f, 0f),
+                    width: 600,
+                    height: 500,
+                    draggable: false);
+                Panel.SetActive(false);
+                Panel.AddComponent<SaveGUIBehaviour>();
+
+                var layout = Panel.AddComponent<VerticalLayoutGroup>();
+                layout.childAlignment = TextAnchor.MiddleCenter;
+                layout.childForceExpandWidth = true;
+                layout.childForceExpandHeight = false;
+                layout.padding = new RectOffset(15, 15, 15, 15);
+                layout.spacing = 20f;
+
+                // Label
+
+                var saveLabel = GUIManager.Instance.CreateText(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_saveblueprint"),
+                    parent: Panel.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0f, 0f),
+                    font: GUIManager.Instance.AveriaSerifBold,
+                    fontSize: 30,
+                    color: GUIManager.Instance.ValheimOrange,
+                    outline: true,
+                    outlineColor: Color.black,
+                    width: 500f,
+                    height: 50f,
+                    addContentSizeFitter: false);
+                saveLabel.GetComponent<Text>().alignment = TextAnchor.UpperCenter;
+                saveLabel.AddComponent<LayoutElement>().preferredHeight = 80f;
+
+                // Name
+
+                var name = new GameObject("Name");
+                name.transform.SetParent(Panel.transform);
+                name.AddComponent<LayoutElement>().preferredHeight = 70f;
+
+                GUIManager.Instance.CreateText(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_name"),
+                    parent: name.transform,
+                    anchorMin: new Vector2(0f, 1f),
+                    anchorMax: new Vector2(0f, 1f),
+                    position: new Vector2(100f, 0f),
+                    font: GUIManager.Instance.AveriaSerif,
+                    fontSize: 20,
+                    color: GUIManager.Instance.ValheimOrange,
+                    outline: true,
+                    outlineColor: Color.black,
+                    width: 150f,
+                    height: 40f,
+                    addContentSizeFitter: false);
+
+                var nameInput = GUIManager.Instance.CreateInputField(
+                    parent: name.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0f, 0f),
+                    contentType: InputField.ContentType.Standard,
+                    placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_name_placeholder"),
+                    fontSize: 20,
+                    width: 400f,
+                    height: 40f);
+
+                Name = nameInput.GetComponent<InputField>();
+                Name.text = bpname;
+
+                // Category
+
+                var cat = new GameObject("Category");
+                cat.transform.SetParent(Panel.transform);
+                cat.AddComponent<LayoutElement>().preferredHeight = 70f;
+
+                GUIManager.Instance.CreateText(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_category"),
+                    parent: cat.transform,
+                    anchorMin: new Vector2(0f, 1f),
+                    anchorMax: new Vector2(0f, 1f),
+                    position: new Vector2(100f, 0f),
+                    font: GUIManager.Instance.AveriaSerif,
+                    fontSize: 20,
+                    color: GUIManager.Instance.ValheimOrange,
+                    outline: true,
+                    outlineColor: Color.black,
+                    width: 150f,
+                    height: 40f,
+                    addContentSizeFitter: false);
+
+                var catInput = GUIManager.Instance.CreateInputField(
+                    parent: cat.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0f, 0f),
+                    contentType: InputField.ContentType.Alphanumeric,
+                    placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_category_placeholder"),
+                    fontSize: 20,
+                    width: 400f,
+                    height: 40f);
+
+                Category = catInput.GetComponent<InputField>();
+                Category.text = BlueprintAssets.CategoryBlueprints;
+
+                // Description
+
+                var desc = new GameObject("Description");
+                desc.transform.SetParent(Panel.transform);
+                desc.AddComponent<LayoutElement>().preferredHeight = 170f;
+
+                GUIManager.Instance.CreateText(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_description"),
+                    parent: desc.transform,
+                    anchorMin: new Vector2(0f, 1f),
+                    anchorMax: new Vector2(0f, 1f),
+                    position: new Vector2(100f, 0f),
+                    font: GUIManager.Instance.AveriaSerif,
+                    fontSize: 20,
+                    color: GUIManager.Instance.ValheimOrange,
+                    outline: true,
+                    outlineColor: Color.black,
+                    width: 150f,
+                    height: 40f,
+                    addContentSizeFitter: false);
+
+                var descInput = GUIManager.Instance.CreateInputField(
+                    parent: desc.transform,
+                    anchorMin: new Vector2(0.5f, 0.5f),
+                    anchorMax: new Vector2(0.5f, 0.5f),
+                    position: new Vector2(0f, 0f),
+                    contentType: InputField.ContentType.Standard,
+                    placeholderText: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_description_placeholder"),
+                    fontSize: 20,
+                    width: 400f,
+                    height: 120f);
+
+                Description = descInput.GetComponent<InputField>();
+                Description.lineType = InputField.LineType.MultiLineNewline;
+
+                // Buttons
+                
+                var buttons = new GameObject("Buttons");
+                buttons.transform.SetParent(Panel.transform);
+                buttons.AddComponent<LayoutElement>().preferredHeight = 80f;
+
+                var okButton = GUIManager.Instance.CreateButton(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_confirm"),
+                    parent: buttons.transform,
+                    anchorMin: new Vector2(0f, 0.5f),
+                    anchorMax: new Vector2(0f, 0.5f),
+                    position: new Vector2(100f, 0f),
+                    width: 100f,
+                    height: 40f);
+                okButton.GetComponent<Button>().onClick.AddListener(OnOk);
+
+                var cancelButton = GUIManager.Instance.CreateButton(
+                    text: LocalizationManager.Instance.TryTranslate("$gui_bpmarket_cancel"),
+                    parent: buttons.transform,
+                    anchorMin: new Vector2(1f, 0.5f),
+                    anchorMax: new Vector2(1f, 0.5f),
+                    position: new Vector2(-100f, 0f),
+                    width: 100f,
+                    height: 40f);
+                cancelButton.GetComponent<Button>().onClick.AddListener(OnCancel);
+
+                Panel.SetActive(true);
+                Name.Select();
+                GUIManager.BlockInput(true);
+            }
+
+            private static void OnOk()
+            {
+                OkAction.Invoke(Name.text.Trim(), Category.text.Trim(), Description.text.Trim());
+                Panel.SetActive(false);
+                Object.Destroy(Panel);
                 GUIManager.BlockInput(false);
             }
 
-            panel.SetActive(true);
-            GUIManager.BlockInput(true);
+            private static void OnCancel()
+            {
+                CancelAction.Invoke();
+                Panel.SetActive(false);
+                Object.Destroy(Panel);
+                GUIManager.BlockInput(false);
+            }
+            
+            private class SaveGUIBehaviour : MonoBehaviour
+            {
+                private void Update()
+                {
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        OnOk();
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Escape))
+                    {
+                        OnCancel();
+                    }
+                }
+            }
         }
     }
 }
